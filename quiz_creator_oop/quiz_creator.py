a3755b3b6f102c20a91428ffea6a7f8cfba3d193
Vince <vince.maano1@gmail.com>
2025-05-27 17:00:35 +0800

    added everything else
    I really am doubting if I'm converting it to OOP correctly

--- quiz_creator_oop/quiz_creator.py
+++ quiz_creator_oop/quiz_creator.py
@@ -2,13 +2,47 @@
 import time
 
 #imports but the animations.py this time
-from animation import spec_print, animated_center
+from animation import Animation
 
 #class
 class QuizCreator:
         #__init__
         def __init__(self, filename):
-		self.filename = filename
-		self.choices = ["a", "b", "c", "d"]
-#def create_quiz
+                self.filename = filename
+                self.choices = ["a", "b", "c", "d"]
+                self.anim = Animation()
 
+        #def create_quiz
+        def create_quiz(self):
+                self.anim.animated_center("loading...")
+                with open(self.filename, "a") as file_writer:
+                        while True:
+                                self.anim.spec_print('Enter a question (type "stop" to finish): ')
+                                question = input()
+                                if question.lower() == "stop":
+                                        break
+
+                                answers = {}
+                                correct = ""
+
+                                for choice in self.choices:
+                                        self.anim.spec_print(f"{choice}. ")
+                                        ans = input()
+                                        self.anim.spec_print("Is this the correct answer? (y/n): ")
+                                        is_correct = input().lower()
+                                        answers[choice] = ans
+                                        if is_correct == "y":
+                                                correct = choice
+
+                                #animation
+                                index = 0
+                                while index < len(question):
+                                        print(f"{self.filename} < {question[index:]}")
+                                        index += 1
+                                        time.sleep(0.1)
+
+                                #write to file
+                                file_writer.write(f"Question: {question}\n")
+                                for key in self.choices:
+                                        file_writer.write(f"{key}) {answers[key]}\n")
+                                file_writer.write(f"Correct answer: {correct}\n\n")
