f2bd5f0cd6fd8505c649a44669717a4b579f44ac
Vince <vince.maano1@gmail.com>
2025-05-27 19:54:08 +0800

    finish main.py
    should be good, just gonna test it one more time and upload the quiz1.txt in this folder

--- quiz_reader_oop/main.py
+++ quiz_reader_oop/main.py
@@ -1,5 +1,10 @@
 #import
+from quiz_runner import QuizRunner
 
 #read the quiz1.txt
-
+quiz = QuizRunner("quiz1.txt")
 #run the quiz
+while True:
+        quiz.run()
+        if not quiz.ask_play_again():
+                break
