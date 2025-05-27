e0bed35f21c65a39170a6399dbdaf972316f398b
Vince <vince.maano1@gmail.com>
2025-05-27 16:46:37 +0800

    added import and class and __init__
    took a while to commit cuz I kinda did the whole code + the other program in vscode

--- quiz_creator_oop/quiz_creator.py
+++ quiz_creator_oop/quiz_creator.py
@@ -1,10 +1,14 @@
 #imports
+import time
 
 #imports but the animations.py this time
+from animation import spec_print, animated_center
 
 #class
-
-#__init__
-
+class QuizCreator:
+        #__init__
+        def __init__(self, filename):
+		self.filename = filename
+		self.choices = ["a", "b", "c", "d"]
 #def create_quiz
 
