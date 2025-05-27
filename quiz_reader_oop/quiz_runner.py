7886bf2830b5a412d01f9155af2c36d8f2229aa4
Vince <vince.maano1@gmail.com>
2025-05-27 19:42:28 +0800

    just the imports and class and __init__ for now as the next def is pretty long

--- quiz_reader_oop/quiz_runner.py
+++ quiz_reader_oop/quiz_runner.py
@@ -1,9 +1,13 @@
 #imports
+import random
+from animations import Animation
 
 #class
-
-#__init__
-
+class QuizRunner:
+        #__init__
+        def __init__(self, filename):
+                self.filename = filename
+                self.anim = Animation()
 #def load questions
 
 #def run
